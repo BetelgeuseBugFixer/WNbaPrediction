e6f706bf4d9094f1ed77f617a8ae365f052da468
Aaron Roth <rotha@cip.ifi.lmu.de>
2025-11-07 17:14:08 +0000

    renamed cols

--- formulas.py
+++ formulas.py
@@ -4,6 +4,13 @@ import pandas as pd
 # ...
 
 def add_offensive_and_defensive_rating_to_df(players_teams_stats):
+    # aggreagte by season plus payer
+    group_cols = ["playerID", "year"]
+    sum_cols=["GP", "GS", "minutes", "points", "oRebounds", "dRebounds", "rebounds", "assists", "steals", "blocks", "turnovers", "PF", "fgAttempted", "fgMade", "ftAttempted", "ftMade", "threeAttempted", "threeMade", "dq", "PostGP", "PostGS", "PostMinutes", "PostPoints", "PostoRebounds", "PostdRebounds", "PostRebounds", "PostAssists", "PostSteals", "PostBlocks", "PostTurnovers", "PostPF", "PostfgAttempted", "PostfgMade", "PostftAttempted", "PostftMade", "PostthreeAttempted", "PostthreeMade", "PostDQ"]
+    agg_dict = {col: "sum" for col in sum_cols}
+    players_teams_stats = players_teams_stats.groupby(group_cols, as_index=False).agg(agg_dict)
+
+
     df = players_teams_stats.copy()
 
     # ... (o resto do teu pré-cálculo fica igual)
@@ -27,18 +34,9 @@ def add_offensive_and_defensive_rating_to_df(players_teams_stats):
     )
 
     # ✅ 1) garantir que existem no df
-    df["o_ind_raw"] = o_ind_raw
-    df["d_ind_raw"] = d_ind_raw
+    df["o_ind"] = o_ind_raw
+    df["d_ind"] = d_ind_raw
 
-    # Helper: z-score por ano a partir de uma Series e do ano
-    def z_by_year(series, year_series):
-        mu = series.groupby(year_series).transform("mean")
-        sd = series.groupby(year_series).transform("std").replace(0, np.nan)
-        return (series - mu) / sd
 
-    # ✅ 2) usar a própria Series agrupada por df["year"]
-    df["o_ind_z"] = z_by_year(df["o_ind_raw"], df["year"]).fillna(0)
-    df["d_ind_z"] = z_by_year(df["d_ind_raw"], df["year"]).fillna(0)
-
-    df["avg_ind"] = 3*(df["o_ind_z"] + 2*df["d_ind_z"]) / 5.0
+    df["avg_ind"] = 3*(df["o_ind"] + 2*df["d_ind"]) / 5.0
     return df
